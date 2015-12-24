191080eddb10c7fc479701d2d18c12f779a2f6d7
vaspoz <vaspoz@mail.ru>
2015-12-24 15:47:07 +0100

    test

--- src/main/java/edu/vaspoz/spittr/config/WebConfig.java
+++ src/main/java/edu/vaspoz/spittr/config/WebConfig.java
@@ -57,7 +57,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     }
 
-
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 
