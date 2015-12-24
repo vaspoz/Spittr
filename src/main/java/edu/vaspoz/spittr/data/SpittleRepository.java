6c0b826f12fd86f21fa36e81d33ca66bd31008e0
Vasilii_Pozdeev <Vasilii_Pozdeev@epam.com>
2015-12-24 14:39:53 +0100

    New spittle repository interface

---
+++ src/main/java/edu/vaspoz/spittr/data/SpittleRepository.java
@@ -0,0 +1,12 @@
+package edu.vaspoz.spittr.data;
+
+import edu.vaspoz.spittr.Spittle;
+
+import java.util.List;
+
+/**
+ * Created by Vasilii_Pozdeev on 24-Dec-15.
+ */
+public interface SpittleRepository {
+    List<Spittle> findSpittles(long max, int count);
+}
