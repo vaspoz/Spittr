926be47a29361e4d1dc90acc1d89ee6098346be7
Vasilii_Pozdeev <Vasilii_Pozdeev@epam.com>
2015-12-24 15:29:44 +0100

    More advanced package scan + mocked SpittleRepository bean

--- src/main/java/edu/vaspoz/spittr/config/RootConfig.java
+++ src/main/java/edu/vaspoz/spittr/config/RootConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * Created by Vasilii_Pozdeev on 23-Dec-15.
  */
 @Configuration
-@ComponentScan(basePackages = {"edu.vaspoz.spittr"},
+@ComponentScan(basePackages = {"edu.vaspoz.spittr.data"},
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
         })
--- src/main/java/edu/vaspoz/spittr/config/WebConfig.java
+++ src/main/java/edu/vaspoz/spittr/config/WebConfig.java
@@ -1,5 +1,7 @@
 package edu.vaspoz.spittr.config;
 
+import edu.vaspoz.spittr.Spittle;
+import edu.vaspoz.spittr.data.SpittleRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +11,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Vasilii_Pozdeev on 23-Dec-15.
  */
-
 @Configuration
 @EnableWebMvc
-@ComponentScan("edu.vaspoz.spittr")
+@ComponentScan("edu.vaspoz.spittr.controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
@@ -26,6 +31,29 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setSuffix(".jsp");
         resolver.setExposeContextBeansAsAttributes(true);
         return resolver;
+
+    }
+
+
+    // Just mock-repo
+    @Bean
+    public SpittleRepository spittleRepository() {
+
+        return new SpittleRepository() {
+            @Override
+            public List<Spittle> findSpittles(long max, int count) {
+                List<Spittle> spittleList = new ArrayList<>();
+                for (int i = 0; i < 10; i++) {
+                    spittleList.add(new Spittle("Spittle " + i, new Date()));
+                }
+                return spittleList;
+            }
+
+            @Override
+            public Spittle findOne(long spittleId) {
+                return new Spittle("Hi! I`m just a spittle with id = " + spittleId, new Date());
+            }
+        };
 
     }
 
